cda32167f2f26fe458521eff9c9758c03423d65c
tjohnson2718 <tjohnson@student.neumont.edu>
2025-05-28 19:44:10 -0600

    Project Setup
    - Main project is setup with directory structure and files that have appropriate names for what they will accomplish
    
    - I have setup a test project to use during the development process.

---
+++ Test/Builder/TaskBuilderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DesignPatternsFinal.Builder;
+
+namespace Test.Builder
+{
+    [TestFixture]
+    public class TaskBuilderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+    }
+}
